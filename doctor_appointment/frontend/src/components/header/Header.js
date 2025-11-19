bb4e537f47c3d86e4e3be9bbff8bc309d0b306ac
Anthony <anthonyfung99@outlook.com>
2025-11-18 22:15:29 -0800

    build react

---
+++ doctor_appointment/frontend/src/components/header/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header({ isLoggedIn }) {
+  return (
+    <nav className="main-header">
+      <div className="logo-section">
+        <Link to="/" className="logo-section">
+          <img src="/img/logo.png" alt="CareConnect Logo" className="logo-icon" />
+          <span className="app-title"><span className="highlight">CareConnect</span></span>
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        {isLoggedIn ? (
+          <>
+            <li><Link to="/appointments">Show Appointments</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/logout">Logout</Link></li>
+          </>
+        ) : (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
